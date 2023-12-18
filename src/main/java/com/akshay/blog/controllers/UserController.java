41611724d43207b9598238487eef63bf21aabe5e
Akshya Chaudhary <akshya.chaudhary@w3villa.com>
2023-12-18 16:13:30 +0530

    Crud apis done for user controller

--- src/main/java/com/akshay/blog/controllers/UserController.java
+++ src/main/java/com/akshay/blog/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.akshay.blog.controllers;
 
+import com.akshay.blog.payloads.ApiResponse;
 import com.akshay.blog.payloads.UserDTO;
 import com.akshay.blog.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,10 +18,32 @@ public class UserController {
     @Autowired
     private UserServices userServices;
 
-    @PostMapping("/")
+    @PostMapping("/create-user")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         UserDTO createdUserDTO = this.userServices.createUser(userDTO);
 
         return new ResponseEntity<>(createdUserDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update-user/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO, @PathVariable("userId") Integer uid) {
+        UserDTO updatedUser = this.userServices.updateUser(userDTO, uid);
+        return  ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/delete-user/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+        this.userServices.deleteUser(userId);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully", true), HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all-users")
+    public ResponseEntity<List<UserDTO>> getAllusers(){
+        return ResponseEntity.ok(this.userServices.getAllUsers());
+    }
+
+    @GetMapping("/get-user-detail/{userId}")
+    public ResponseEntity<UserDTO> getSingleUser(@PathVariable Integer userId){
+        return ResponseEntity.ok(this.userServices.getuserById(userId) );
     }
 }
--- src/test/java/com/akshay/blog/BloggingBackendApplicationTests.java
+++ src/test/java/com/akshay/blog/BloggingBackendApplicationTests.java
@@ -1,13 +1,26 @@
 package com.akshay.blog;
 
+import com.akshay.blog.reporsitories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BloggingBackendApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	public void repoTest(){
+		String className = this.userRepository.getClass().getName();
+		String packName = this.userRepository.getClass().getPackageName();
+
+		System.out.println(className);
+		System.out.println(packName);
 	}
 
 }
