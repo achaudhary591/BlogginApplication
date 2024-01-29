5e46cf346f0f1ff734abef6877a58703b4daba01
Akshya Chaudhary <akshya.chaudhary@w3villa.com>
2024-01-29 18:32:05 +0530

    added security dependency

---
+++ src/main/java/com/akshay/blog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.akshay.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    /**
+     * @param http 
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.
+                csrf().disable();
+    }
+}
